8b234ff7448b677e7aeef881a223c324ad5655da
irfan khan <ikha4949@gmail.com>
2025-08-16 20:30:01 +0530

    Add initial diner app files and assets
    Introduces the main files for a diner menu app, including HTML, CSS, JS logic, menu data, and image assets. Implements menu rendering, cart functionality, order completion, and payment modal.

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        image : "images/pizza.png",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        emoji: "🍕",
+        id : "764f6239-2869-4c18-b19d-159113560720"
+    },
+    {
+        name: "Hamburger",
+        image: "images/burger.png",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: "51c2696e-e9c4-461b-8bb3-4418523e28f0"
+    },
+        {
+        name: "Beer",
+        image: "images/beer.png",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id : "435f504b-1094-48c2-8804-633d32fb473c"
+    }
+]
---
+++ index.js
@@ -0,0 +1,122 @@
+import { menuArray } from '/data'
+
+function dinerHTML(){
+    let menu = ''
+    menuArray.map((food) => {    
+        menu += `    
+            <div class="product-card">
+                <div class="image">
+                    <img src="${food.image}" class="product">
+                </div>
+                <div class="info">
+                    <div class="product-names">
+                        <ul class="product-name">${food.name}</ul>
+                    </div>
+                    <div class="product-ingredients-info">
+                        <ul class="ingredients">
+                            <li>${food.ingredients},</li>
+                        </ul>
+                    </div>
+                    <div class="product-prices">
+                        <ul class="product-price">
+                            <li>${food.price}$</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <button class="plusButton" data-add="${food.id}">
+                <i class="fa-solid fa-plus" data-add="${food.id}"></i>
+            </button>
+        `
+    })
+    return menu
+}
+
+function render(){
+    document.getElementById('section').innerHTML = dinerHTML(menuArray)
+}
+render()
+
+let cart = []
+
+document.addEventListener('click', function(e) {
+    if (e.target.dataset.add) {
+        const foodId = e.target.dataset.add 
+        const food = menuArray.find(item => item.id == foodId) 
+        if(food) { 
+            cart.push(food)
+            renderCart()
+        }
+    }
+    if (e.target.dataset.remove) {
+        const removeId = e.target.dataset.remove
+        cart = cart.filter(item => item.id != removeId) 
+        renderCart()
+    }
+})
+
+function renderCart() {
+    if (cart.length === 0) {
+        document.getElementById('section-part-one').innerHTML = ""
+        return
+    }
+    let completeOrderHTML = `
+        <main id="section-one">
+            <p class="your-order">Your order</p>
+            <div class="summary">
+    `
+    cart.map((item) => {
+        completeOrderHTML += `
+            <div class="row">
+                <span class="cart-product">${item.name}</span>
+                <button class="removeButton" data-remove="${item.id}">remove</button>
+                <span class="product-price">$${item.price}</span>
+            </div>
+        `
+    })
+    const total = cart.reduce((sum, item) => sum + item.price, 0)
+    completeOrderHTML += `
+            <div class="line"></div>
+            <div class="row">
+                <span class="subtotal-label">Total price</span>
+                <span class="subtotal-price">$${total}</span>
+            </div>
+            <button class="complete-order-btn" type="submit" data-submit="complete-order-btn">Complete order</button>
+        </div>
+    </main>
+    `
+    document.getElementById('section-part-one').innerHTML = completeOrderHTML
+}
+
+document.addEventListener('click',function(e){
+    if(e.target.dataset.submit){
+        let submitOrderHtml = `<form id = "consent-form">
+                    <p class="form-heading">Enter card details</p>
+                    <div class="form-inputs">
+                    <input type="text" name="fullName" id = "fullName" placeholder="Enter your name" minlength="4" required/>
+                    <input type="number" name="cardNumber" placeholder="Enter card number" minlength="4" required/>
+                     <input type="password" name="CVV" placeholder="Enter your CVV" minlength="3" required/>
+                    </div>
+                    <div class="form-btn">
+                    <button type="submit" id="form-modal-btn" data-pay="form-modal-btn">Pay</button>
+                    </div>
+                </form>`
+                document.getElementById('popup-modal').innerHTML = submitOrderHtml
+    } 
+})
+
+document.addEventListener('click', function(e) {
+    if (e.target.dataset.pay) {
+        e.preventDefault()  
+        const consentForm = document.getElementById('consent-form')
+        const consentFormData = new FormData(consentForm)
+        const fullName = consentFormData.get('fullName')
+        let finalMessage = `
+            <div class="final-msg">
+                <p class="userName">Thanks, ${fullName}! Your order is on its way!</p>
+            </div>
+        `
+        document.getElementById('popup-modal').style.display = 'none' 
+        document.getElementById('section-part-one').innerHTML = finalMessage  
+    }
+})
